983ce7fafee7f6395015af55ada17f6b106f9d30
2018302080133 <fw2396@columbia.edu>
2023-07-18 18:09:54 -0400

    authorization

--- seeds/index.js
+++ seeds/index.js
@@ -19,6 +19,7 @@ const seedDB = async () => {
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const camp = new Campground({
+            author: '64b46c81e5537fa1536c99f0',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://i.insider.com/5b4e1386afb7751b008b45d1?width=1000&format=jpeg&auto=webp',
