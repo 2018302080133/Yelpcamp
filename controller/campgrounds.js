1b44d6898214ce37468dfbdddf27709689ed694a
2018302080133 <fw2396@columbia.edu>
2023-07-24 15:47:29 -0400

    maps

--- controller/campgrounds.js
+++ controller/campgrounds.js
@@ -1,6 +1,10 @@
 const Campground = require('../models/campground');
 const {cloudinary} = require('../cloudinary');
 
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
+const mapBoxToken = process.env.MAPBOX_TOKEN;
+const geoCoder = mbxGeocoding({accessToken: mapBoxToken});
+
 
 module.exports.index = async (req, res) => {
     const campgrounds = await Campground.find({});
@@ -13,6 +17,11 @@ module.exports.renderNewForm = (req, res) => {
 
 module.exports.createCampground = async (req, res) => {
     const campground = new Campground(req.body.campground);
+    const geoData = await geoCoder.forwardGeocode({
+        query: req.body.campground.location,
+        limit: 1
+    }).send();
+    campground.geometry = geoData.body.features[0].geometry;
     campground.images = req.files.map(f => ({url: f.path, filename: f.filename}));
     campground.author = req.user._id;
     await campground.save();
